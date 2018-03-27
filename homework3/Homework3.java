4e6ea9f85c9a5ccc8be48d29d53e38c4ed999890
Shyla Clark <shylaclark@gmail.com>
2018-03-27 13:09:13 -0500

    Cleaned up whitespace

--- homework3/Homework3.java
+++ homework3/Homework3.java
@@ -1,132 +1,106 @@
 /**
-* CSCI 1583 
-* Homework Three
-* Shyla Clark
-* Feb 14
-**/
+ * Shyla Clark
+ * CSCI 1583
+ * Homework 3: Student Grade Calculator
+ * Feb 14, 2014
+ **/
 
-import java.util.Scanner; // create and initialize scanner
+import java.util.Scanner;
 
 public class Homework3 {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in); 
+        Scanner input = new Scanner(System.in);
 		String studentName = "";
 		int menu = 0;
 		int menu2 = 0;
 		
-		int hwGrade = 0; // grade
-		int hwCounter = 0; // increment counter
-		int hwTotal = 0; // total
+        // Homeworks
+		int hwGrade = 0;
+		int hwCounter = 0;
+		int hwTotal = 0;
 		
-		int qGrade = 0; // grade
-		int qCounter = 0; // increment counter
-		int qTotal = 0; // total
+        // Quizzes
+		int qGrade = 0;
+		int qCounter = 0;
+		int qTotal = 0;
 		
-		int tGrade = 0; // grade
-		int tCounter = 0; // increment counter
-		int tTotal = 0; // total
+        // Tests
+		int tGrade = 0;
+		int tCounter = 0;
+		int tTotal = 0;
 		
-		double average; // number with decimal for average
-			
-		System.out.println( "Enter student name: " ); // prompt
-      	studentName = input.next(); // read first name from user
-			
+        double average;
 
-		if ( studentName != null ) {
-			System.out.println( "Type 1 to enter homework grades, 2 to enter quiz grades, 3 to enter test grades or -1 to quit. " ); // prompt
-			menu = input.nextInt(); // read menu choice
+		System.out.println("Enter student name: ");
+      	studentName = input.next();
+        
+		if (studentName != null) {
+			System.out.println("Type 1 to enter homework grades, 2 to enter quiz grades, 3 to enter test grades or -1 to quit.");
+			menu = input.nextInt();
 			
-			if ( menu == 1 ) {
-								
+            // Enter homework grades
+			if (menu == 1) {
 				do { 
-					System.out.println( "Type 1 to enter homework grade or -1 to quit. " );
-					menu2 = input.nextInt(); // read menu choice
-				
-					if ( menu2 == 1 ) {
-						System.out.println( "Enter homework grade: " ); // homework prompt
-			        	hwGrade = input.nextInt(); // read homework grade
-						
+					System.out.println("Type 1 to enter homework grade or -1 to quit.");
+					menu2 = input.nextInt();
+                    
+					if (menu2 == 1) {
+						System.out.println("Enter homework grade: ");
+			        	hwGrade = input.nextInt();
 						hwTotal = hwTotal + hwGrade;
 			        	hwCounter = hwCounter + 1;
-				
-					} // end if
-					else if ( menu2 == -1 ) {
-				
-						// termination phase
-						average = (double) hwTotal / hwCounter; // get average
-			
-	            		System.out.printf( "\nTotal of the %d grades entered is %d\n", hwCounter, hwTotal );
-	            		System.out.printf( "Average for homework is %.2f\n", average );
-				
-					} // end else if
-					
-				} while ( menu2 != -1 ); // end do while
-					
-			
-			} // end if
-			
-			else if ( menu == 2 ) {
-				
+					}
+                    else if (menu2 == -1) {
+						average = (double) hwTotal / hwCounter;
+	            		System.out.printf("\nTotal of the %d grades entered is %d\n", hwCounter, hwTotal);
+	            		System.out.printf("Average for homework is %.2f\n", average);
+					}
+				}
+                while (menu2 != -1); // Exit-condition
+			}
+            
+            // Calculate quiz grades
+            else if (menu == 2) {
 				do {
-					System.out.println( "Type 1 to enter quiz grade or -1 to quit. " );
-					menu2 = input.nextInt(); // read menu choice
+					System.out.println("Type 1 to enter quiz grade or -1 to quit.");
+					menu2 = input.nextInt();
 				
-					if ( menu2 == 1 ) {
-						System.out.println( "Enter quiz grade: " ); // quiz prompt
-		        		qGrade = input.nextInt(); // read quiz grade
-
-					
+					if (menu2 == 1) {
+						System.out.println("Enter quiz grade: ");
+		        		qGrade = input.nextInt();
 						qTotal = qTotal + qGrade;
 		        		qCounter = qCounter + 1;
-						
-					} // end if
-					
-					else if ( menu2 == -1 ) {
+					}
+                    else if (menu2 == -1) {
+						average = (double) qTotal / qCounter;
+            			System.out.printf("\nTotal of the %d grades entered is %d\n", qCounter, qTotal);
+            			System.out.printf("Average for quizzes is %.2f\n", average);
+					}
+				}
+                while (menu2 != -1);
+			}
 				
-						// termination phase
-						average = (double) qTotal / qCounter; // get average
-			
-            			System.out.printf( "\nTotal of the %d grades entered is %d\n", qCounter, qTotal );
-            			System.out.printf( "Average for quizzes is %.2f\n", average );
-				
-					} // end else if
-					
-				} while ( menu2 != -1 ); // end do while
-				
-			} // end else if
-				
-			else if ( menu == 3 ) {
-				
+			else if (menu == 3) {
 				do {
-					System.out.println( "Type 1 to enter test grade or -1 to quit. " );
-					menu2 = input.nextInt(); // read menu choice
+					System.out.println("Type 1 to enter test grade or -1 to quit.");
+					menu2 = input.nextInt();
 				
-					if ( menu2 == 1 ) {
-						System.out.print( "Enter test grade: " ); // test prompt
-		        		tGrade = input.nextInt(); // read test grade
-
-					
+					if (menu2 == 1) {
+						System.out.print("Enter test grade: ");
+		        		tGrade = input.nextInt();
 						tTotal = tTotal + tGrade;
 		        		tCounter = tCounter + 1;
-				
-					} // end if
-					
-			 		else if ( menu2 == -1 ) {
-				
-						// termination phase
-						average = (double) tTotal / tCounter; // get average
-			
-            			System.out.printf( "\nTotal of the %d grades entered is %d\n", tCounter, tTotal );
-            			System.out.printf( "Average for tests is %.2f\n", average );
-				
-					} // end else if
-				} while ( menu2 != -1 ); // end do while
-				
-			} // end else if
-			
-		} // end student if
-						
-    } // end main
-} // end public class hw3fix
+					}
+			 		else if (menu2 == -1) {
+						average = (double) tTotal / tCounter;
+            			System.out.printf("\nTotal of the %d grades entered is %d\n", tCounter, tTotal);
+            			System.out.printf("Average for tests is %.2f\n", average);
+					}
+				}
+                while (menu2 != -1);
+			}
+		}
+    }
+}
