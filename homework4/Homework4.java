1b210d8f34a3615503aee7a6419664efb0547abd
Shyla Clark <shylaclark@gmail.com>
2018-03-27 14:07:28 -0500

    Update Homework4.java

--- homework4/Homework4.java
+++ homework4/Homework4.java
@@ -10,7 +10,7 @@ import java.util.*; // create and initialize scanner
 
 public class Homework4 {
 	
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
 		
         Scanner input = new Scanner(System.in); 
 		
@@ -24,110 +24,95 @@ public class Homework4 {
 		do {
 			choice = Menu(); 
 			
-			if ( choice == 1 ) {
-			enterGrades( hwGrades ); // call enterGrades method and pass hwGrades
+			if (choice == 1) {
+                enterGrades(hwGrades);
 			}
-			else if ( choice == 2 ) {
+			else if (choice == 2) {
 				enterGrades (qGrades); 
 			}	
-			else if ( choice == 3 ) {	
-				enterGrades ( tGrades );
+			else if (choice == 3) {
+				enterGrades (tGrades);
 			}	
-			/**
-				else if ( choice == 4 ) {
-				System.out.println( " " ); 
-				menu = input.nextInt(); 
-				}
-				*/
-			else if ( choice == 5 ) {
-				System.out.println( getAverage ( hwGrades ) );
+
+			else if (choice == 5) {
+				System.out.println(getAverage(hwGrades));
 			}
-			else if ( choice == 6 ) {
-				System.out.println( getAverage ( qGrades ) );
+			else if (choice == 6) {
+				System.out.println(getAverage(qGrades));
 				
 			}
-			else if ( choice == 7 ) {
-				System.out.println( getAverage ( tGrades ) );
+			else if (choice == 7) {
+				System.out.println( getAverage( tGrades));
 			}
-			else if ( choice == 8 ) {
+			else if (choice == 8) {
 				System.out.println( totalAverage ( hwGrades, qGrades, tGrades ) );
 			}
-		} while ( choice >= 0); // end do while			
-    } // end main method
+		} while (choice >= 0); // Exit-condition
+    }
 	
-	public static int Menu() { 
+	public static int Menu() {
+		Scanner input = new Scanner(System.in);
+        int choice = 0;
+
+		System.out.println("Choose\n1. Input grades\n2. Get averages\n3. Quit");
+        choice = input.nextInt();
 		
-		Scanner input = new Scanner( System.in );
-			int choice = 0;
-		
-		System.out.println( "Choose\n1. Input grades\n2. Get averages\n3. Quit" );
+		if (choice == 1) {
+			System.out.println("Choose\n1. Input homework grade\n2. Input quiz grades\n3. Input test grades\n4. Return to main");
 			choice = input.nextInt();
-		
-		if ( choice == 1 ) {
-			System.out.println( "Choose\n1. Input homework grade\n2. Input quiz grades\n3. Input test grades\n4. Return to main " );
-			choice = input.nextInt();
-		} // end else if choice == 1
-			
-		else if ( choice == 2 ) {
-			System.out.println( "Choose\n1. Get homework average\n2. Get quiz average\n3. Get test average\n4. Total average\n5. Return to main " );
+		}
+		else if (choice == 2) {
+			System.out.println("Choose\n1. Get homework average\n2. Get quiz average\n3. Get test average\n4. Total average\n5. Return to main ");
 			choice = input.nextInt();
 			
-			if ( choice == 1 ) {
+			if (choice == 1) {
 				choice = 5;
 			}
-			else if ( choice == 2 ) {
+			else if (choice == 2) {
 				choice = 6;
 			}
-			else if ( choice == 3 ) {
+			else if (choice == 3) {
 				choice = 7;
 			}
-			else if ( choice == 4 ) {
+			else if (choice == 4) {
 				choice = 8;
 			}
-			else if ( choice == 5 ) {
+			else if (choice == 5) {
 				choice = 4;
 			}
-		} // end else if choice == 2
-			
-		else if ( choice == 3 ) {
+		}
+        else if (choice == 3) {
 			choice = -1;
-		} // end else if choice == 3
-		
+		}
 		return choice;
-	} // end method Menu
+	}
 	
-	public static void enterGrades( List<Double> grades ) {
+	public static void enterGrades(List<Double> grades) {
         Scanner input = new Scanner(System.in); 
 		double grade = 0.0;
 		do {
-			System.out.println( "Enter grade or type -1 to quit." );
+			System.out.println("Enter grade or type -1 to quit.");
 			grade = input.nextDouble();
-			if ( grade >= 0.0 ) {
-				grades.add( grade );
-			} // end if statement
-		} while ( grade >= 0.0 );
-	} // end method enterGrades
+			if (grade >= 0.0) {
+				grades.add(grade);
+			}
+		} while (grade >= 0.0);
+	}
 	
-	
-	public static double getAverage( List<Double> grades ) {
+	public static double getAverage(List<Double> grades) {
 		double average = 0.0;
-		for ( int index = 0; index < grades.size(); index++ ) { // use for when you have certain number in mind
+		for (int index = 0; index < grades.size(); index++) { // use for when you have certain number in mind
 			average = average + grades.get(index); // average += grades.get(index);
-		} // end for
+		}
 		average = average / grades.size();
 		return average;
-	} // end method getAverage
+	}
 		
-	public static double totalAverage( List<Double> hwGrades, List<Double> qGrades, List<Double> tGrades ) {
-		double hwAverage = getAverage( hwGrades );
-		double qAverage = getAverage( qGrades );
-		double tAverage = getAverage( tGrades );
-		
-		double totalAverage = ( hwAverage + qAverage + tAverage ) / 3.0;
-		
+	public static double totalAverage(List<Double> hwGrades, List<Double> qGrades, List<Double> tGrades) {
+		double hwAverage = getAverage(hwGrades);
+		double qAverage = getAverage(qGrades);
+		double tAverage = getAverage(tGrades);
+		double totalAverage = (hwAverage + qAverage + tAverage) / 3.0;
 		return totalAverage;
-		
-	} // end method totalAverage
-
-	
-} // end public class Homework4
+	}
+}
